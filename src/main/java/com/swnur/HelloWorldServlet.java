47ff206ad01b257c6e322f640ca5abc31be9e903
Nurbek Shukan <shukannurbek@gmail.com>
2024-11-04 10:38:49 +0100

    Init commit

---
+++ src/main/java/com/swnur/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package com.swnur;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("Hello world");
+    }
+}
