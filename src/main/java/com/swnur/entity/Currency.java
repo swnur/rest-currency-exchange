6c46a63431e4fe16320a940435212256e63e9785
Nurbek Shukan <shukannurbek@gmail.com>
2024-11-04 17:52:38 +0100

    Create Currency model

---
+++ src/main/java/com/swnur/entity/Currency.java
@@ -0,0 +1,21 @@
+package com.swnur.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Currency {
+    private Long id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public Currency(String code, String fullName, String sign) {
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+}
